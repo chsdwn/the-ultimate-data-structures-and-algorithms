0320e27bc3a094cbe85cc037e8ad7c6750ff368f
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-09 14:18:12 +0300

    5-O(n^2)

--- Program.cs
+++ Program.cs
@@ -7,34 +7,22 @@ namespace the_ultimate_data_structures_and_algorithms
         static void Main(string[] args)
         {
             int[] numbers = { 1, 2, 3 };
-            string[] names = { "Ali", "Veli" };
 
-            // O(n+2) => O(n)
-            Console.WriteLine(""); // O(1)
-            foreach (int number in numbers) // O(n)
-            {
-                Console.WriteLine(number);
-            }
-            Console.WriteLine(""); // O(1)
-
-            // O(2n) => O(n)
-            foreach (int number in numbers) // O(n)
-            {
-                Console.WriteLine(number);
-            }
+            // O(n + n^3) => O(n^3)
             foreach (int number in numbers) // O(n)
             {
                 Console.WriteLine(number);
             }
 
-            // O(n + m) => O(n)
             foreach (int number in numbers) // O(n)
             {
-                Console.WriteLine(number);
-            }
-            foreach (string name in names) // O(m)
-            {
-                Console.WriteLine(name);
+                foreach (int number2 in numbers) // O(n)
+                {
+                    foreach (int number3 in numbers) // O(n)
+                    {
+                        Console.WriteLine($"{number}, {number2}, {number3}");
+                    }
+                }
             }
         }
     }
