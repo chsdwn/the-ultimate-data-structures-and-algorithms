81e69c82edb9a8ba76fea36f8dd416ea062526fb
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-09 14:13:35 +0300

    4-O(n)

--- Program.cs
+++ Program.cs
@@ -7,9 +7,35 @@ namespace the_ultimate_data_structures_and_algorithms
         static void Main(string[] args)
         {
             int[] numbers = { 1, 2, 3 };
-            // O(1)
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
+            string[] names = { "Ali", "Veli" };
+
+            // O(n+2) => O(n)
+            Console.WriteLine(""); // O(1)
+            foreach (int number in numbers) // O(n)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(""); // O(1)
+
+            // O(2n) => O(n)
+            foreach (int number in numbers) // O(n)
+            {
+                Console.WriteLine(number);
+            }
+            foreach (int number in numbers) // O(n)
+            {
+                Console.WriteLine(number);
+            }
+
+            // O(n + m) => O(n)
+            foreach (int number in numbers) // O(n)
+            {
+                Console.WriteLine(number);
+            }
+            foreach (string name in names) // O(m)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
