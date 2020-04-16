412306c6b6cec9a5a4d58e562072c8e863e6e3b4
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-04-16 13:09:15 +0300

    7-Solution A Basix Implementation

--- Stack/BalancedExpressions.cs
+++ Stack/BalancedExpressions.cs
@@ -5,33 +5,22 @@ namespace the_ultimate_data_structures_and_algorithms.Stack
 {
     public class BalancedExpressions
     {
-        public bool isBalanced(string str)
+        public bool isBalanced(string input)
         {
-            if (str == null || str.Length == 0) throw new NullReferenceException();
-
-            var opens = "{[(<";
-            var closes = "}])>";
-
             var stack = new Stack<char>();
 
-            foreach (var ch in str)
+            foreach (var ch in input)
             {
-                for (int i = 0; i < opens.Length; i++)
+                if (ch.Equals('(')) stack.Push(ch);
+
+                if (ch.Equals(')'))
                 {
-                    if (ch == closes[i])
-                    {
-                        if (stack.Count == 0) return false;
-
-                        var item = stack.Pop();
-                        if (item != opens[i]) return false;
-                    }
-
-                    else if (ch == opens[i])
-                        stack.Push(opens[i]);
+                    if (stack.Count == 0) return false;
+                    stack.Pop();
                 }
             }
 
-            return true;
+            return stack.Count == 0 ? true : false;
         }
     }
 }
